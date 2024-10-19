5af4978f8279420dd6902cdd58955c4abc0d499a
drake <drake@GlizzyGobbler>
2024-10-19 11:15:38 -0700

    sbdone

--- src/_01_StringBuilder/_03_StringBuilder.java
+++ src/_01_StringBuilder/_03_StringBuilder.java
@@ -4,18 +4,26 @@ package _01_StringBuilder;
 public class _03_StringBuilder {
     
     public static String append(String str, char[] characters) {
-        return null;
+    	StringBuilder sb = new StringBuilder(str);
+    	sb.append(characters);
+        return sb.toString();
     }
     
     public static String reverse(String str) {
-        return null;
+    	StringBuilder sb = new StringBuilder(str);
+    	sb.reverse();
+        return sb.toString();
     }
     
     public static String insert(String str, int index, char newChar) {
-        return null;
+    	StringBuilder sb = new StringBuilder(str);
+    	sb.insert(index, newChar);
+        return sb.toString();
     }
     
     public static String delete(String str, int startIndex, int endIndex) {
-        return null;
+    	StringBuilder sb = new StringBuilder(str);
+    	sb.delete(startIndex, endIndex);
+        return sb.toString();
     }
 }
